a66cddaa4dfc23aa70710b07e3471d19812eaf4e
ivandelvalle <tec.ivandelvaller@outlook.com>
2021-03-23 00:52:54 -0600

    Add store endpoint

--- LinkProject/routes/api.php
+++ LinkProject/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\LinkController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +18,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/', [LinkController::class, 'index']);
+Route::prefix('/link')->group(function() {
+    Route::post('/store', [LinkController::class, 'store']);
+    Route::put('/{id}', [LinkController::class, 'update']);
+    Route::delete('/{id}', [LinkController::class, 'destroy']);
 });
