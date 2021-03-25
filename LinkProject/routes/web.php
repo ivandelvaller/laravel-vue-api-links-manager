ce7856f1120f14a5a3c0686a7d47e593289714fe
ivandelvalle <tec.ivandelvaller@outlook.com>
2021-03-24 23:36:38 -0600

    fixed routing

--- LinkProject/routes/web.php
+++ LinkProject/routes/web.php
@@ -13,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', 'SpaController@index')->where('any', '.*');
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/{any}', 'App\Http\Controllers\SpaController@index')->where('any', '.*');
